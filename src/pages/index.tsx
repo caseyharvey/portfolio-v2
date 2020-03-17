989363f709de2aac85c62b26d0e70c9889d13f9f
caseyharvey <caseyharvey1@gmail.com>
2020-03-18 10:25:39 +1100

    Update paths to components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,36 @@
 import React from "react"
 // import { Link } from "gatsby"
 
-import Layout from "../components/Layout/layout"
+import Layout from "../components/Layout/Layout"
 // import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, ipsa! Ad
+      optio, nisi laudantium quod reprehenderit quae voluptas, sapiente ab
+      beatae aut, est deserunt! Praesentium illum eius ab libero eos itaque quam
+      esse! Maxime porro magnam illo adipisci officia modi cupiditate explicabo,
+      repudiandae minima odit? Obcaecati accusamus dignissimos repellendus
+      nihil, culpa repellat consectetur sint laborum velit in numquam
+      perferendis laboriosam recusandae atque illum officia rerum sequi error
+      ratione aliquam maxime hic. Accusantium aspernatur recusandae hic quas
+      esse minima dolor ullam atque ratione nostrum autem obcaecati qui
+      asperiores, incidunt blanditiis perferendis quibusdam amet molestias
+      aliquid, dignissimos eveniet id beatae rerum? Et beatae maxime, fugiat
+      vero aperiam sint veritatis illo ipsam, esse suscipit quod velit ea eaque
+      obcaecati, facilis numquam! Enim et ratione est fugit libero architecto
+      molestiae autem nemo dolor, harum, odit ducimus magni, asperiores quod
+      eligendi quam. Expedita quidem veniam nesciunt eius temporibus
+      perferendis, sunt eaque eum deserunt dolore sit. Ipsum quasi quam debitis
+      numquam a! Soluta eligendi harum distinctio sequi iure rem aliquam
+      nesciunt earum natus corporis non temporibus, adipisci, vel dolorum ipsa
+      nam! Nesciunt, ipsa sequi. Sunt libero eligendi, dolorum illum officia
+      sequi iste voluptas repellendus ab aspernatur ipsum amet non dolore, harum
+      illo perspiciatis fugit vel ullam?
+    </div>
   </Layout>
 )
 

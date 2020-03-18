ccedb93448ef46ae0fce85f9b33a0c441e6d734a
caseyharvey <caseyharvey1@gmail.com>
2020-03-18 11:04:09 +1100

    Refactor

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,35 +2,28 @@ import React from "react"
 // import { Link } from "gatsby"
 
 import Layout from "../components/Layout/Layout"
+import Text from "../components/Text/Text"
 // import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <div>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, ipsa! Ad
-      optio, nisi laudantium quod reprehenderit quae voluptas, sapiente ab
-      beatae aut, est deserunt! Praesentium illum eius ab libero eos itaque quam
-      esse! Maxime porro magnam illo adipisci officia modi cupiditate explicabo,
-      repudiandae minima odit? Obcaecati accusamus dignissimos repellendus
-      nihil, culpa repellat consectetur sint laborum velit in numquam
-      perferendis laboriosam recusandae atque illum officia rerum sequi error
-      ratione aliquam maxime hic. Accusantium aspernatur recusandae hic quas
-      esse minima dolor ullam atque ratione nostrum autem obcaecati qui
-      asperiores, incidunt blanditiis perferendis quibusdam amet molestias
-      aliquid, dignissimos eveniet id beatae rerum? Et beatae maxime, fugiat
-      vero aperiam sint veritatis illo ipsam, esse suscipit quod velit ea eaque
-      obcaecati, facilis numquam! Enim et ratione est fugit libero architecto
-      molestiae autem nemo dolor, harum, odit ducimus magni, asperiores quod
-      eligendi quam. Expedita quidem veniam nesciunt eius temporibus
-      perferendis, sunt eaque eum deserunt dolore sit. Ipsum quasi quam debitis
-      numquam a! Soluta eligendi harum distinctio sequi iure rem aliquam
-      nesciunt earum natus corporis non temporibus, adipisci, vel dolorum ipsa
-      nam! Nesciunt, ipsa sequi. Sunt libero eligendi, dolorum illum officia
-      sequi iste voluptas repellendus ab aspernatur ipsum amet non dolore, harum
-      illo perspiciatis fugit vel ullam?
-    </div>
+    <Text>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sed
+        praesentium odio eveniet omnis iure sint perspiciatis tempore nam!
+        Voluptatibus, quam aperiam officiis repellendus quia nam labore
+        consequuntur harum mollitia quo sunt laborum, nulla vitae, consequatur
+        odio deserunt saepe molestiae rem.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam sed
+        labore repellendus doloremque? Cupiditate incidunt iste vel sequi
+        consequuntur porro cum quisquam fugiat expedita vero odio soluta saepe
+        praesentium pariatur, officia maiores illo molestias voluptas.
+      </p>
+    </Text>
   </Layout>
 )
 

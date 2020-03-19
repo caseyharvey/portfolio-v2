39502c473a29a8f500a1403c611761452498200a
caseyharvey <caseyharvey1@gmail.com>
2020-03-19 21:18:06 +1100

    More projects added

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -2,7 +2,7 @@ import React from "react"
 
 import Content from "../components/Content/Content"
 import Heading from "../components/Heading/Heading"
-import Buttons from "../components/Buttons/Buttons"
+import Links from "../components/Links/Links"
 import Layout from "../components/Layout/Layout"
 import SEO from "../components/seo"
 
@@ -14,7 +14,24 @@ const ProjectsPage = () => (
       tagLine="dollar value timer"
       info="react redux redux-form"
     />
-    <Buttons
+    <Links
+      app="https://timeismoney.caseys.tech/"
+      code="https://github.com/caseyharvey/time-is-money/tree/master/src"
+    />
+    <Content>
+      <p>
+        I built this app as a way to keep track of time while I study. We all
+        know how precious time is but we don't always use it for things that
+        matter. This app can help you clarify whats important in your life by
+        giving it a dollar value.
+      </p>
+    </Content>
+    <Heading
+      heading="timeIsMoney"
+      tagLine="dollar value timer"
+      info="react redux redux-form"
+    />
+    <Links
       app="https://timeismoney.caseys.tech/"
       code="https://github.com/caseyharvey/time-is-money/tree/master/src"
     />

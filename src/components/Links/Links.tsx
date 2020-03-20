13e08d12fad71e61f6fe36b76097a58e9dd7095d
caseyharvey <caseyharvey1@gmail.com>
2020-03-20 19:29:27 +1100

    Taget _blank add

--- src/components/Links/Links.tsx
+++ src/components/Links/Links.tsx
@@ -7,17 +7,16 @@ interface Props {
   linkTwo: any
   textOne: string
   textTwo: string
-  target?: string
 }
 
 function Links({ linkOne, linkTwo, textOne, textTwo, target }: Props) {
   return (
     <div className="links-container">
       <div className="links">
-        <a href={linkOne} target={target} rel="noopener noreferrer">
+        <a href={linkOne} target="_blank" rel="noopener noreferrer">
           {textOne}
         </a>
-        <a href={linkTwo} target={target} rel="noopener noreferrer">
+        <a href={linkTwo} target="_blank" rel="noopener noreferrer">
           {textTwo}
         </a>
       </div>

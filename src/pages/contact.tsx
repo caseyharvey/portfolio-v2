54e2a89b66230b1f5a59fe24419467632a1ccf25
caseyharvey <caseyharvey1@gmail.com>
2020-03-20 13:10:45 +1100

    Add Section and links

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,22 +1,39 @@
 import React from "react"
 
-import Heading from "../components/Heading/Heading"
 import ContactForm from "../components/ContactForm/ContactForm"
-import Layout from "../components/Layout/Layout"
+import Heading from "../components/Heading/Heading"
+import Section from "../components/Section/Section"
 import Content from "../components/Content/Content"
+import Layout from "../components/Layout/Layout"
+import Links from "../components/Links/Links"
 import SEO from "../components/seo"
 
 const ContactPage = () => (
   <Layout>
     <SEO title="Projects" />
-    <Heading
-      heading="Contact"
-      tagLine={<a href="mailto:im@caseys.tech?">im@caseys.tech</a>}
-      info="Copy to clipboard"
-    />
-    <Content>
-      <ContactForm />
-    </Content>
+    <Section>
+      <Heading
+        heading="Contact"
+        tagLine="im@caseys.tech"
+        // tagLine={<a href="mailto:im@caseys.tech?">im@caseys.tech</a>}
+        info="copy email to clipboard"
+      />
+      <Links
+        linkOne="https://github.com/caseyharvey"
+        textOne="open in mail"
+        linkTwo="https://www.linkedin.com/in/casey-harvey-b931a71a0/"
+        textTwo="copy email"
+      />
+      <Links
+        linkOne="https://github.com/caseyharvey"
+        textOne="GitHub"
+        linkTwo="https://www.linkedin.com/in/casey-harvey-b931a71a0/"
+        textTwo="LinkedIn"
+      />
+      <Content>
+        <ContactForm />
+      </Content>
+    </Section>
   </Layout>
 )
 

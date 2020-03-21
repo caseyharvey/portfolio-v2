8a119a59cceef4fe6ed76ff6075ddb430e80bfa3
caseyharvey <caseyharvey1@gmail.com>
2020-03-21 20:36:50 +1100

    Add themeContext for light and dark mode

--- src/components/Layout/layout.tsx
+++ src/components/Layout/layout.tsx
@@ -1,6 +1,9 @@
 import React, { ReactNode } from "react"
-// import { useStaticQuery, graphql } from "gatsby"
+
+import ThemeContext from "../../context/ThemeContext"
 import Nav from "../Nav/Nav"
+import { faSun, faMoon } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import "./layout.sass"
 
 interface Props {
@@ -8,23 +11,34 @@ interface Props {
 }
 
 const Layout = ({ children }: Props) => {
-  // const data = useStaticQuery(graphql`
-  //   query SiteTitleQuery {
-  //     site {
-  //       siteMetadata {
-  //         title
-  //       }
-  //     }
-  //   }
-  // `)
-
   return (
-    <>
-      <Nav />
-      <main className="main-container">{children}</main>
-    </>
+    <ThemeContext.Consumer>
+      {theme => (
+        <div className={theme.dark ? "dark" : "light"}>
+          <Nav />
+          <main className="main-container">
+            <div className="light-dark-btn" onClick={theme.toggleDark}>
+              {theme.dark ? (
+                <FontAwesomeIcon icon={faSun} />
+              ) : (
+                <FontAwesomeIcon icon={faMoon} />
+              )}
+            </div>
+            {children}
+          </main>
+        </div>
+      )}
+    </ThemeContext.Consumer>
   )
 }
+
+// return (
+//   <>
+//     <Nav />
+//     <main className="main-container">{children}</main>
+//   </>
+// )
+// }
 
 export default Layout
 // siteTitle={data.site.siteMetadata.title}

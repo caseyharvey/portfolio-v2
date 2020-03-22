a8a169aeef7c4f4a7802569c52b28aeec615c23d
caseyharvey <caseyharvey1@gmail.com>
2020-03-22 11:58:47 +1100

    Remove dead code

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,9 @@
 module.exports = {
   siteMetadata: {
     title: `caseys.tech`,
+    author: `Casey Harvey`,
+    url: `https://www.caseys.tech`,
     description: `The developer portfolio of Casey Harvey.`,
-    author: `@caseyharvey`,
   },
   plugins: [
     {
@@ -22,17 +23,6 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    // {
-    //   resolve: `gatsby-plugin-manifest`,
-    //   options: {
-    //     name: `gatsby-starter-default`,
-    //     short_name: `starter`,
-    //     start_url: `/`,
-    //     background_color: `#663399`,
-    //     theme_color: `#663399`,
-    //     display: `minimal-ui`,
-    //     icon: `src/images/gatsby-icon.png`,
-    //   },
-    // },
+    `gatsby-plugin-manifest`,
   ],
 }

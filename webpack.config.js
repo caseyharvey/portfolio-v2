458b801e3bb3c3268413c1fd3b67255de270ae1c
caseyharvey <caseyharvey1@gmail.com>
2020-03-22 16:36:04 +1100

    Add sass loader

--- webpack.config.js
+++ webpack.config.js
@@ -9,8 +9,8 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
+        test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
       },
     ],
   },

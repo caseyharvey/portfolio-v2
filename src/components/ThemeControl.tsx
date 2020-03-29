1b7fbeff32f4fb271f4d61f17bb2d3cfa4448431
caseyharvey <caseyharvey1@gmail.com>
2020-03-29 13:48:00 +1100

    Change darkmode toggle icon and adjust style

--- src/components/ThemeControl.tsx
+++ src/components/ThemeControl.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { faMoon, faSun } from "@fortawesome/free-solid-svg-icons"
+import { faAdjust } from "@fortawesome/free-solid-svg-icons"
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { ThemeToggler } from "gatsby-plugin-dark-mode"
@@ -18,11 +18,7 @@ function ThemeControl() {
             className="light-dark-btn"
             onClick={() => toggleTheme(theme === "dark" ? "light" : "dark")}
           >
-            {theme === "dark" ? (
-              <FontAwesomeIcon icon={faSun} />
-            ) : (
-              <FontAwesomeIcon icon={faMoon} />
-            )}
+            <FontAwesomeIcon icon={faAdjust} />
           </div>
         )}
       </ThemeToggler>
